40d107094fcf17320a850466399438ecbf4e1b61
nluu175 <nluu@ualberta.ca>
2022-01-03 23:07:14 -0700

    Added Token

--- index.js
+++ index.js
@@ -5,27 +5,59 @@ const http = require("http");
 const server = http.createServer(app);
 const socketio = require("socket.io");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.use(cors);
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-// app.use("cors");
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const io = socketio(server, {
-  cors: {},
-  origin: "*",
-  methods: ["GET", "POST"],
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-// TODO: Move these variables to utils/config.js
-const PORT = process.env.PORT || 3001;
+app.post("/login", (req, res) => {
+  console.log("a person logged in");
+  res.send({
+    token: "test123",
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
 
+// ! Socket IO;
+// * Move this to other file if possible
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  logger.info("a user connected");
+
+  socket.on("message", (data) => {
+    logger.info(data);
+  });
+
+  socket.on("disconnect", () => {
+    logger.info("A user disconnected");
+  });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running at http://localhost:${config.PORT}`);
 });
